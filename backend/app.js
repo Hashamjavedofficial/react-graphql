ac3641b69189f828ef7a500d51714d324175d6bb
Hasham Javed <hasham.hasham1@gmail.com>
2021-11-16 22:16:43 +0500

    graphql schema created

--- backend/app.js
+++ backend/app.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const expressGraphQL = require('express-graphql');
 
+const schema = require('./schema/schema')
+
 const app = express();
 const PORT = 4000
 
 const {graphqlHTTP} = expressGraphQL;
 
 app.use('/graphql',graphqlHTTP({
+    schema: schema,
     graphiql: true
 }))
 
--- backend/schema/schema.js
+++ backend/schema/schema.js
@@ -1,6 +1,12 @@
 const graphql = require('graphql');
+const _ = require('lodash');
 
-const {GraphQLObjectType,GraphQLString,GraphQLInt} = graphql
+const {GraphQLObjectType,GraphQLString,GraphQLInt,GraphQLSchema} = graphql
+
+const users = [
+    {id:23,firstName: 'Hasham Javed',age: 26},
+    {id:54, firstName: 'Saif Javed',age: 20}
+]
 
 const UserType = new GraphQLObjectType({
     name:'User',
@@ -10,3 +16,23 @@ const UserType = new GraphQLObjectType({
         age: {type:GraphQLInt},
     }
 })
+
+const RootQuery = new GraphQLObjectType({
+    name:'RootQueryType',
+    fields: {
+        user : {
+            type: UserType,
+            args: {id : {type: GraphQLString}},
+            resolve(parentValue,args){
+                //do database query inside this function
+                return _.find(users,{id:args.age})
+            }
+        }
+    }
+})
+
+const graphqlSchema = new GraphQLSchema({
+    query: RootQuery,
+})
+
+module.exports = graphqlSchema
